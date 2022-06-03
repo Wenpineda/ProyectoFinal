e5e485db65b6d6f2ff8cce58f946a30b57034083
Wenpineda <cp0428032021@unab.edu.sv>
2022-06-02 22:48:23 -0600

    Proyecto-cafe

---
+++ InicioMenu-main/InicioMenu-main/menuYinicio/menuYinicio/Controllers/InicioController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace menuYinicio.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+     
+        public ActionResult inicio()
+        {
+            return View();
+        }
+
+        public ActionResult nosotros()
+        {
+            return View();
+        }
+
+        public ActionResult menu()
+        {
+            return View();
+        }
+
+        public ActionResult factura()
+        {
+            return View();
+        }
+    }
+}
